ac0999e1f2da7a5a8602409dea72fd2c42d2b1de
Maksym Sydorenko <maksym.sy96@gmail.com>
2024-12-02 09:24:41 +0200

    chore: have done get cats module

---
+++ get_cats_info/get_cats_info.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import typing
+
+def get_cats_info(path:Path) -> typing.Dict[str, str|int] | None:
+    try:
+        with open(path,'r',encoding='utf-8') as file:
+            lines = file.read().strip().split('\n')
+            result_list = []
+
+            if not lines:
+                return None
+
+            for line in lines:
+                id,name,age = line.strip().split(',')
+                cat = {"id":id,"name":name,"age":age}
+                result_list.append(cat)
+
+        return result_list
+    except FileNotFoundError:
+        print(f"File not found: {path}")
+    except IOError as e:
+        print(f"Error reading file: {e}")
+    return None
+
+
+path = Path('cats.txt')
+print(get_cats_info(path))
