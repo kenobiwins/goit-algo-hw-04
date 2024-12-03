fa4fd3508fc99274d11b7a4fec8576466f967428
Maksym Sydorenko <maksym.sy96@gmail.com>
2024-12-03 16:30:30 +0200

    fix: fix total salary count

--- total_salary/total_salary.py
+++ total_salary/total_salary.py
@@ -2,13 +2,16 @@ from pathlib import Path
 import typing
 
 
-def count_salaries(salaries: typing.List[str]) -> typing.List[int]:
+def count_salaries(salaries: typing.List[str]) -> typing.List[float]:
     total_salaries = []
 
     for line in salaries:
         try:
+            if not line.strip():
+                continue
+
             _, salary = line.split(",")
-            salary = int(salary.strip())
+            salary = float(salary.strip())
             if salary < 0:
                 raise ValueError(f"Salary not able to be negative: {salary}")
             total_salaries.append(salary)
@@ -17,25 +20,35 @@ def count_salaries(salaries: typing.List[str]) -> typing.List[int]:
     return total_salaries
 
 
-def total_salary(path: Path) -> typing.Tuple[int, int] | None:
+def total_salary(path: Path) -> typing.Tuple[float, float] | typing.Tuple[None, None]:
     try:
         with open(path, "r", encoding="utf-8") as file:
             lines = file.read().strip().split("\n")
-
+            
             if not lines:
-                return None
+                return (None, None)
 
             salaries = count_salaries(lines)
+
+            if not salaries:
+                return (None, None)
+
             total_salary = sum(salaries)
-            average_salary = total_salary // len(salaries)
+            average_salary = total_salary / len(salaries)
             return (total_salary, average_salary)
     except FileNotFoundError:
         print(f"File not found: {path}")
     except IOError as e:
         print(f"Error reading file: {e}")
-    return None
+    return (None, None)
 
 
 path = Path("salary_file.txt")
 total, average = total_salary(path)
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+if total is None or average is None:
+    print("Неможливо вирахувати загальну та середню зп")
+else:
+    print(
+        f"Загальна сума заробітної плати: {total:.2f}, Середня заробітна плата: {average:.2f}"
+    )
